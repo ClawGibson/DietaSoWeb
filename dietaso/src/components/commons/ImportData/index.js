629a6b7b28016a30545c4957c2433fdd64961633
Gamaliel Bernal <gamalielbj@gmail.com>
2021-07-04 21:32:30 -0500

    Routes & Side Menu
    - Routes created.
    - SideMenu with 2 routes [Principal & Importar datos].

---
+++ dietaso/src/components/commons/ImportData/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DataLayout from '../../layouts/DataLayout';
+
+const index = () => {
+  return <DataLayout>Importar datos</DataLayout>;
+};
+
+export default index;
--- dietaso/src/components/layouts/SideMenu/index.js
+++ dietaso/src/components/layouts/SideMenu/index.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Menu } from 'antd';
+import { CloudUploadOutlined, HomeOutlined } from '@ant-design/icons';
+import Routes from '../../../routes/routes';
+import PathRoutes from '../../../routes/PathRoutes';
+
+import './SideMenu.scss';
 
 const SideMenu = () => {
-  return <div>Menú de navegación.</div>;
+  const [onHover, setOnHover] = useState(true);
+
+  const history = useHistory();
+
+  const handleHistory = (route) => {
+    history.replace(`${route}`);
+  };
+
+  const handleHover = () => {
+    setOnHover(!onHover);
+  };
+
+  return (
+    <div
+      className='sideMenuContainer'
+      onMouseLeave={handleHover}
+      onMouseEnter={handleHover}
+    >
+      <Menu
+        classname='menuContainer'
+        inlineCollapsed={onHover}
+        mode='inline'
+        defaultSelectedKeys={[Routes.Principal]}
+        defaultSelectedKeys={['import']}
+      >
+        <Menu.Item
+          className='menuItem'
+          key={Routes.Principal}
+          icon={<HomeOutlined />}
+          onClick={() => handleHistory(Routes.Principal)}
+        >
+          Principal
+        </Menu.Item>
+        <Menu.Item
+          className='menuItem'
+          key={Routes.ImportarDatos}
+          icon={<CloudUploadOutlined />}
+          onClick={() => handleHistory(Routes.ImportarDatos)}
+        >
+          Importar datos
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
 };
 
 export default SideMenu;
