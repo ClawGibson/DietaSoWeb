c86751e8d2c46cfec0ba7bf6d29a8c2a9fa5b164
Gamaliel Bernal <gamalielbj@gmail.com>
2021-07-11 21:59:58 -0500

    Uploader file

--- dietaso/src/components/commons/ImportData/index.js
+++ dietaso/src/components/commons/ImportData/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 
 import DataLayout from '../../layouts/DataLayout';
 
-const index = () => {
+const Index = () => {
+  const [file, setFile] = useState('');
+  const props = {
+    action: '//jsonplaceholder.typicode.com/posts/',
+    listType: 'picture',
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        console.log(`${info.file.name} file uploaded successfully`);
+        setFile(info.file);
+      } else if (info.file.status === 'error') {
+        console.log(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
   return (
     <DataLayout>
-      <Upload maxCount={1} accept={'.xlsx'}>
+      <Upload {...props} maxCount={1} accept={'.xlsx'}>
         <Button icon={<UploadOutlined />}>Seleccionar archivo</Button>
       </Upload>
+      ,
     </DataLayout>
   );
 };
 
-export default index;
+export default Index;
--- dietaso/src/components/layouts/DataLayout/index.js
+++ dietaso/src/components/layouts/DataLayout/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './DataLayout.scss';
 
 const DataLayout = ({ children }) => {
-  return <div className="dataLayout">{children}</div>;
+  return <div className='dataLayout'>{children}</div>;
 };
 
 export default DataLayout;
