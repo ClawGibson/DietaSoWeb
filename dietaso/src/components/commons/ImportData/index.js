73d5ae62fca368c94280fec213a6400497b5b3f5
Gamaliel Bernal <gamalielbj@gmail.com>
2021-07-15 22:27:09 -0500

    First try to send the selected file to back

--- dietaso/src/components/commons/ImportData/index.js
+++ dietaso/src/components/commons/ImportData/index.js
@@ -6,29 +6,64 @@ import { UploadOutlined } from '@ant-design/icons';
 import DataLayout from '../../layouts/DataLayout';
 
 const Index = () => {
-  const [file, setFile] = useState('');
-  const props = {
-    action: '//jsonplaceholder.typicode.com/posts/',
-    listType: 'picture',
-    onChange(info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === 'done') {
-        console.log(`${info.file.name} file uploaded successfully`);
-        setFile(info.file);
-      } else if (info.file.status === 'error') {
-        console.log(`${info.file.name} file upload failed.`);
-      }
-    },
+  const axios = require('axios');
+  const [file, setFile] = useState(null);
+
+  const onChange = async (info) => {
+    const nextState = {};
+    switch (info.file.status) {
+      case 'uploading':
+        nextState.selectedFileList = [info.file];
+        console.log(`Subiendo el archivo ${info.file.name}...`);
+        break;
+      case 'done':
+        nextState.selectedFile = info.file;
+        console.log(`Archivo ${info.file.name} subido exitosamente`);
+        console.log('nextState', nextState);
+        break;
+
+      default:
+        // error or removed
+        nextState.selectedFile = null;
+        console.log(`El archivo ${info.file.name} no se pudo subir`);
+    }
+    await setFile(nextState);
   };
 
+  const dummyRequest = ({ file, onSuccess }) => {
+    setTimeout(() => {
+      onSuccess('ok');
+    }, 0);
+  };
+
+  const handleImport = async () => {
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MGJkMDc3YjNmZGM0ZjI1YmMzZGE3MjMiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MjYzOTY3MTcsImV4cCI6MTY1Nzk1NDMxN30.uZ0YzVKYa1XDpUN2sQo_OndqfqcNiMv_OMS9yBc1j6Q';
+    try {
+      const response = await axios.post(
+        'https://dietasoapiv1.herokuapp.com/api/v2/equivalencias',
+        file,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      console.log('response', response);
+    } catch (err) {
+      console.log('Ocurrió un error al querer importar los datos: ', err);
+    }
+  };
+  console.log(typeof file);
   return (
     <DataLayout>
-      <Upload {...props} maxCount={1} accept={'.xlsx'}>
+      <Upload
+        customRequest={dummyRequest}
+        maxCount={1}
+        accept={'.xlsx'}
+        onChange={onChange}
+      >
         <Button icon={<UploadOutlined />}>Seleccionar archivo</Button>
       </Upload>
-      ,
+      <Button onClick={() => handleImport()}>Importar archivo</Button>
     </DataLayout>
   );
 };
--- dietaso/src/components/views/Home/index.js
+++ dietaso/src/components/views/Home/index.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DataLayout from '../../layouts/DataLayout';
+import { Input, Button } from 'antd';
 
 const Home = () => {
-  return <DataLayout>Página principal</DataLayout>;
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const axios = require('axios');
+
+  const setData = async () => {
+    const data = {
+      email: user.toString(),
+      contrasena: password.toString(),
+    };
+
+    try {
+      const response = await axios.post(
+        'https://dietasoapiv1.herokuapp.com/api/v2/usuarios/login',
+        data
+      );
+
+      if (!response.data) console.log('No se pudo');
+      else console.log('Si se pudo', response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <DataLayout>
+      <Input placeholder='Usuario' onChange={(e) => setUser(e.target.value)} />
+      <Input
+        placeholder='Contraseña'
+        type='password'
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={() => setData()}>Submit</Button>
+    </DataLayout>
+  );
 };
 
 export default Home;
