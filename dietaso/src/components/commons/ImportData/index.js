ba1ab4c0df93eed4c0827b1960959924ba3851a8
Aram <boiso.ska8@gmail.com>
2021-07-20 20:03:03 -0500

    first commit

--- dietaso/src/components/commons/ImportData/index.js
+++ dietaso/src/components/commons/ImportData/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import DataLayout from '../../layouts/DataLayout';
+import { Upload, message, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+
+const props = {
+  name: 'file',
+  action: 'http:example.com',
+  headers: {
+    authorization: 'token'
+  },
+  onChange(info) {
+
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded succesfully.`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`)
+    }
+
+  },
+};
+
+
 
 const index = () => {
-  return <DataLayout>Importar datos</DataLayout>;
+  return (
+    <DataLayout>
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />} className="btn">Click to upload</Button>
+      </Upload>
+    </DataLayout>
+  );
 };
 
 export default index;
--- dietaso/src/components/layouts/MainLayout/index.js
+++ dietaso/src/components/layouts/MainLayout/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import './MainLayout.scss';
 
 const MainLayout = ({ children }) => {
+
+  console.log("Main Layout Children: ", children);
+
+
   return <div className="mainLayout">{children}</div>;
 };
 
