c81d8bf4af81dc6539a4abf74a563823e5a914a6
Alfonso A <alfonsoanguianoarcila10@gmail.com>
2021-10-05 14:13:33 -0500

    Correcion y nombres de los alimentos

---
+++ dietaso/src/components/commons/FoodComponents/PropertiesComponent.js
@@ -0,0 +1,18 @@
+
+
+const PropertiesComponent = () => {
+    return (
+        <>
+            <div class="props">                    
+                <div class="head_props"><h1 id="title">Propiedades</h1></div>
+                <div class="data_props">    </div>
+                <div class="preparaciones">
+                    <div class="tags">  </div>
+                    <div class="add_tag">   </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default PropertiesComponent;
