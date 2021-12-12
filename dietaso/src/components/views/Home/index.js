ff929f8fab2c2e092deb9e7a6198c4dc9471eb90
Gama <gamalielbj@gmail.com>
2021-12-12 09:45:41 -0600

    Quick fixes

--- dietaso/src/components/views/Home/index.js
+++ dietaso/src/components/views/Home/index.js
@@ -1,47 +1,71 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { React, useEffect, useState } from "react";
-import UserCard from "../../commons/UserCard/UserCard";
-import apiURL from "../../../axios/axiosConfig";
+import { React, useEffect, useState } from 'react';
+
+import { useSelector } from 'react-redux';
+import { Row, Col, message, Spin } from 'antd';
+
+import UserCard from '../../commons/UserCard/UserCard';
+
+import apiURL from '../../../axios/axiosConfig';
 
 // Antd imports
-import { Row, Col } from "antd";
 
 // SCSS files
-import "./index.scss";
+import './index.scss';
 
 const Home = () => {
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  const getUsers = async () => {
-    try {
-      const res = await apiURL.get("/usuarios");
+    const { token } = useSelector((state) => state.authorizationStore);
 
-      setUsers(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    useEffect(() => {
+        getUsers();
+        return () => {
+            setUsers([]);
+            setLoading(true);
+        };
+    }, []);
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+    const getUsers = async () => {
+        try {
+            if (token && token !== '') {
+                const { data } = await apiURL.get('/informacionUsuarios');
+                setUsers(data);
+                setLoading(false);
+            } else {
+                message.info('Refresque la página para obtener los usuarios');
+            }
+        } catch (error) {
+            setLoading(false);
+            console.groupCollapsed('[Home.js] getUsers()');
+            console.error(error);
+            console.groupEnd();
+        }
+    };
 
-  return (
-    <div className="users-container">
-      <div className="title-container">
-        <h2>Usuario registrados</h2>
-      </div>
-      <div>
-        <Row gutter={[16, 16]} className="usercard-container">
-          {users.map((user) => (
-            <Col span={6} key={user.id}>
-              <UserCard user={user} />
-            </Col>
-          ))}
-        </Row>
-      </div>
-    </div>
-  );
+    return (
+        <div className='users-container'>
+            <div className='title-container'>
+                <h2>Usuario registrados</h2>
+            </div>
+            <div>
+                <Row gutter={[16, 16]} className='usercard-container'>
+                    {(loading && <Spin size='large' />) ||
+                        users.map(
+                            (user) =>
+                                user.nombre &&
+                                !user.nombre.includes(
+                                    '- Selecione una opción -'
+                                ) && (
+                                    <div key={user.id}>
+                                        <UserCard user={user} />
+                                    </div>
+                                )
+                        )}
+                </Row>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
