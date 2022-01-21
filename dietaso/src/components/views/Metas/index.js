e38d16023d2949a7fae378fbbe8b9a5bf06e73d7
Brodilli <brodilli_@hotmail.com>
2022-01-21 17:32:27 -0600

    Avance de las metas, falta hacerlo bonito con css

--- dietaso/src/components/views/Metas/index.js
+++ dietaso/src/components/views/Metas/index.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Col, Input, Row, Button, Modal, Select } from 'antd';
+import { addAuthorizationAction } from '../../../redux/actions/authorizationAction';
+import apiURL from '../../../axios/axiosConfig';
 import {
     PlusOutlined,
     ExclamationCircleOutlined,
@@ -11,7 +13,51 @@ import {
 
 //import './reminders.scss';
 
+
+
 const Metas = () => {
+    const [objetivo, setObjetivo] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [categoriaDeSostenibilidad, setCategoriaDeSostenibilidad] = useState('');
+    const [metas, setMetas] = useState([]);
+    let getValue = '';
+    useEffect(() => {
+
+        getMetas();
+
+        return () => {
+            setMetas([]);
+
+        };
+    }, []);
+    const getMetas = async () => {
+        try {
+            const { data } = await apiURL.get('/metas');
+            console.log(data);
+            setMetas(data);
+
+        } catch (error) {
+
+            console.groupCollapsed('Error xd');
+            console.error(error);
+            console.groupEnd();
+
+        }
+
+    };
+    function obtenerCategoria(categoria) {
+        setCategoriaDeSostenibilidad(categoria);
+    }
+    const postMetas = async () => {
+        const meta = { objetivo, descripcion, categoriaDeSostenibilidad };
+
+        console.log(meta);
+        try {
+            const response = await apiURL.post('/Metas', meta);
+        } catch (error) {
+            console.error(error);
+        }
+    }
     //ADVERT
     const { confirm } = Modal;
     function showDeleteConfirm() {
@@ -38,6 +84,7 @@ const Metas = () => {
     };
 
     const handleOk = () => {
+        postMetas();
         setIsModalVisible(false);
     };
 
@@ -54,6 +101,7 @@ const Metas = () => {
     }
     return (
         <div class='main-R'>
+
             <Row>
                 <Col span={22} style={{ padding: 16 }}>
                     <h1> Recordatorios</h1>
@@ -68,172 +116,29 @@ const Metas = () => {
                 </Col>
             </Row>
 
-            <Card title='Todas las Metas'>
-                <div class='sc_metas'>
-                    <Card
-                        type='inner'
-                        title={
-                            <Row>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        icon={<GlobalOutlined />}
-                                    />
-                                </Col>
-                                <Col span={18} style={{ padding: 5 }}>
-                                    <h4 class='plistaR'>Recordatorios</h4>
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showModal}
-                                        icon={<EditOutlined />}
-                                    />
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showDeleteConfirm}
-                                        icon={<DeleteOutlined />}
-                                    />
-                                </Col>
-                            </Row>
-                        }>
-                        Este es un recordatorio global
+            {
+                metas.length > 0 &&
+                metas.map((meta) =>
+                    <Card title={meta.objetivo}>
+                        <div class='sc_metas'>
+                            {`${meta.descripcion}\n${meta.categoriaDeSostenibilidad}`}
+                        </div>
                     </Card>
-                    <Card
-                        style={{ marginTop: 16 }}
-                        type='inner'
-                        title={
-                            <Row>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        icon={<UserOutlined />}
-                                    />
-                                </Col>
-                                <Col span={18} style={{ padding: 5 }}>
-                                    <h4 class='plistaR'>Recordatorios</h4>
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showModal}
-                                        icon={<EditOutlined />}
-                                    />
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showDeleteConfirm}
-                                        icon={<DeleteOutlined />}
-                                    />
-                                </Col>
-                            </Row>
-                        }>
-                        Este es un recordatorio
-                    </Card>
-                    <Card
-                        style={{ marginTop: 16 }}
-                        type='inner'
-                        title={
-                            <Row>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        icon={<GlobalOutlined />}
-                                    />
-                                </Col>
-                                <Col span={18} style={{ padding: 5 }}>
-                                    <h4 class='plistaR'>Recordatorios</h4>
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showModal}
-                                        icon={<EditOutlined />}
-                                    />
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showDeleteConfirm}
-                                        icon={<DeleteOutlined />}
-                                    />
-                                </Col>
-                            </Row>
-                        }>
-                        Este es un recordatorio global
-                    </Card>
-                    <Card
-                        style={{ marginTop: 16 }}
-                        type='inner'
-                        title={
-                            <Row>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        icon={<UserOutlined />}
-                                    />
-                                </Col>
-                                <Col span={18} style={{ padding: 5 }}>
-                                    <h4 class='plistaR'>Recordatorios</h4>
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showModal}
-                                        icon={<EditOutlined />}
-                                    />
-                                </Col>
-                                <Col span={2} style={{ padding: 5 }}>
-                                    <Button
-                                        style={{}}
-                                        type='primary'
-                                        shape='circle'
-                                        onClick={showDeleteConfirm}
-                                        icon={<DeleteOutlined />}
-                                    />
-                                </Col>
-                            </Row>
-                        }>
-                        Este es un recordatorio
-                    </Card>
-                </div>
-            </Card>
+                )
+            }
+
 
             <Modal
                 title='Agregar una nueva Meta'
                 visible={isModalVisible}
-                onCancel={handleCancel}>
+                onCancel={handleCancel}
+                onOk={handleOk}>
                 <Row>
                     <Col span={6} style={{ padding: 16 }}>
                         <p>Objetivo:</p>
                     </Col>
                     <Col span={18} style={{ padding: 16 }}>
-                        <Input placeholder='Objetivo de la Meta' />
+                        <Input placeholder='Objetivo de la Meta' onChange={(e) => setObjetivo(e.target.value)} />
                     </Col>
                 </Row>
                 <Row>
@@ -244,7 +149,7 @@ const Metas = () => {
                         <TextArea
                             placeholder='Descripción de la Meta'
                             autoSize
-                        />
+                            onChange={(e) => setDescripcion(e.target.value)} />
                         <div style={{ margin: '24px 0' }} />
                     </Col>
                 </Row>
@@ -253,11 +158,11 @@ const Metas = () => {
                         <p>Categoria de Sostenibilidad:</p>
                     </Col>
                     <Col span={6} style={{ padding: 16 }}>
-                        <Select defaultValue='cultura' style={{ width: 120 }}>
+                        <Select id="categoriaMeta" defaultValue='cultura' style={{ width: 120 }} onChange={obtenerCategoria}>
                             <Option value='cultura'>Cultura</Option>
                             <Option value='sociedad'>Sociedad</Option>
                             <Option value='Economia'>Economía</Option>
-                            <Option value='ambiental'>Ambiental</Option>   
+                            <Option value='ambiental'>Ambiental</Option>
                             <Option value='nutricional'>Nutricional</Option>
                         </Select>
                     </Col>
