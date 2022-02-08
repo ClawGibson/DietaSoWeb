12aeda3d9e42866bd548117fe775908866c897ef
Gama <gamalielbj@gmail.com>
2022-02-08 17:45:02 -0600

    Cleanup

--- dietaso/src/redux/rootReducers.js
+++ dietaso/src/redux/rootReducers.js
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux';
 
 import authorizationReducer from './reducers/authorizationReducer';
-import counter from './reducers/counter';
 
 const rootReducer = combineReducers({
     authorizationStore: authorizationReducer,
-    counter: counter,
-    
 });
 
 export default rootReducer;
