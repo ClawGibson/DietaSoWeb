3233daa95e335b9e4479e1f302ea170b8485312f
Gama <gamalielbj@gmail.com>
2022-03-24 12:55:30 -0600

    Update index.js

--- dietaso/src/utils/index.js
+++ dietaso/src/utils/index.js
@@ -1,5 +1,29 @@
 import dayjs from 'dayjs';
 
+export const waitFor = (ms) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+export function capitilizeWord(word) {
+    if (word) {
+        const lower = word.toLowerCase();
+        return word.charAt(0).toUpperCase() + lower.slice(1);
+    }
+    return '';
+}
+
+export const returnArrayToString = (array) => {
+    try {
+        if (Array.isArray(array)) {
+            return array.join(', ');
+        }
+    } catch (error) {
+        console.groupCollapsed('[returnArrayToString]');
+        console.error(error);
+        console.groupEnd();
+        return '';
+    }
+};
+
 export const stringArrayToNumberArray = (array) => {
     try {
         if (Array.isArray(array)) {
