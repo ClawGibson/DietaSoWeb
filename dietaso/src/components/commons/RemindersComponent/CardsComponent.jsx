a8158d07463738c2bd5aba80db4445539171363b
Gama <gamalielbj@gmail.com>
2022-03-30 13:21:16 -0600

    Quick fix

--- dietaso/src/components/commons/RemindersComponent/CardsComponent.jsx
+++ dietaso/src/components/commons/RemindersComponent/CardsComponent.jsx
@@ -20,90 +20,85 @@ import {
     UserOutlined,
     GlobalOutlined,
 } from '@ant-design/icons';
+import moment from 'moment';
 import apiURL from '../../../axios/axiosConfig';
 
 const CardsComponent = () => {
     const [ isModalVisible, setIsModalVisible ] = useState(false);
     const [ initialData, setInitialData ] = useState([]);
     const [ list, setList ] = useState([]);
-    
-    const [listUsers, setlistUsers] = useState([]);
-    const [listUsersPut, setlistUsersput] = useState([]);
-    const [recorUp, setRecorUp] = useState([]);
-    const [recorDel, setRecorDel] = useState([]);
-    const [titulo, setTitulo] = useState("");
-    const [msj, setMsj] = useState("");
-    const [categoria, setCategoria] = useState("");
+
+    const [ listUsers, setlistUsers ] = useState([]);
+    const [ listUsersPut, setlistUsersput ] = useState([]);
+    const [ recorUp, setRecorUp ] = useState([]);
+    const [ recorDel, setRecorDel ] = useState([]);
+    const [ titulo, setTitulo ] = useState('');
+    const [ msj, setMsj ] = useState('');
+    const [ categoria, setCategoria ] = useState('');
     const { RangePicker } = DatePicker;
-    const [fecha, setFecha] = useState([]);
+    const [ fecha, setFecha ] = useState([]);
     const { TextArea } = Input;
     const { confirm } = Modal;
     const { Search } = Input;
     const { Option } = Select;
-    const [global, setGlobal] = useState(false);
-    const [seleccionado, setSeleccionado] =useState("");
-    
+    const [ global, setGlobal ] = useState(false);
+    const [ seleccionado, setSeleccionado ] = useState('');
 
     useEffect(() => {
         fetchData();
-        console.log(seleccionado?.msj ?? msj)
+        console.log(seleccionado?.msj ?? msj);
     }, []);
 
-    const showModal = async(id) => {
+    const showModal = async (id) => {
         setIsModalVisible(true);
         setSeleccionado(id);
         try {
             const { data } = await apiURL.get('/informacionUsuarios');
             setlistUsers(data);
-            
-            
+
             console.log(listUsers);
-          } catch (error) {
-              message.error(`Error: ${error.message}`);
-          }
+        } catch (error) {
+            message.error(`Error: ${error.message}`);
+        }
     };
 
     const handleOk = async () => {
         setIsModalVisible(false);
-        
+
         try {
-          const reminder = {
-            "usuarios": 
-              listUsersPut
-            ,
-            //hora y fecha
-            "titulo": seleccionado?.titulo ?? titulo,
-            "mensaje": seleccionado?.msj ?? msj,
-            "categoria": seleccionado?.categoria ?? categoria,
-            "dias": [
-              {
-                "day": "martes",
-                "activo": false
-              }
-            ],
-            "fecha": fecha,
-            "global":  global
-          };
-          const response = await apiURL.patch(`/recordatorios/${seleccionado._id}`,reminder);
-          console.log(response);
-          window.location.reload()
-          
-          console.log("actualizado");
-            
+            const reminder = {
+                usuarios: listUsersPut,
+                //hora y fecha
+                titulo: seleccionado?.titulo ?? titulo,
+                mensaje: seleccionado?.msj ?? msj,
+                categoria: seleccionado?.categoria ?? categoria,
+                dias: [
+                    {
+                        day: 'martes',
+                        activo: false,
+                    },
+                ],
+                fecha: fecha,
+                global: global,
+            };
+            const response = await apiURL.patch(
+                `/recordatorios/${seleccionado._id}`,
+                reminder
+            );
+            console.log(response);
+            window.location.reload();
+
+            console.log('actualizado');
         } catch (error) {
             message.error(`Error: ${error.message}`);
         }
     };
     const handleCancel = () => {
-    setIsModalVisible(false);
-    window.location.reload()
+        setIsModalVisible(false);
+        window.location.reload();
     };
-   
- 
 
-    
     function showDeleteConfirm(recordatorio) {
-        
         confirm({
             title: '¿Estás seguro de que quieres eliminar?',
             icon: <ExclamationCircleOutlined />,
@@ -111,8 +106,8 @@ const CardsComponent = () => {
             okText: 'Si',
             okType: 'danger',
             cancelText: 'No',
-            onOk(){
-            /*const deleteRecor = async ()=> {
+            onOk() {
+                /*const deleteRecor = async ()=> {
                 console.log('OK');
                 try {
                     const id="61fdd2c42300b300044f745d";
@@ -123,26 +118,27 @@ const CardsComponent = () => {
                     message.error(`Error: ${error.message}`);
                 }
             },*/
-            const deleteRecor = async ()=> {
-                console.log('OK');
-                try {
-                   // console.log('OK');
-                    
-                    //const id="61fdd2c42300b300044f745d";
-                    //const { id } = await apiURL.delete('/recordatorios?id=${61fdd2c42300b300044f745d}');
-                    const dlt = await apiURL.delete(`/recordatorios/${recordatorio._id}`);
-                    console.log(dlt);
-                    window.location.reload()
-                } catch (error) {
-                    message.error(`Error: ${error.message}`);
-                }
-            };
-            deleteRecor()
+                const deleteRecor = async () => {
+                    console.log('OK');
+                    try {
+                        // console.log('OK');
+
+                        //const id="61fdd2c42300b300044f745d";
+                        //const { id } = await apiURL.delete('/recordatorios?id=${61fdd2c42300b300044f745d}');
+                        const dlt = await apiURL.delete(
+                            `/recordatorios/${recordatorio._id}`
+                        );
+                        console.log(dlt);
+                        window.location.reload();
+                    } catch (error) {
+                        message.error(`Error: ${error.message}`);
+                    }
+                };
+                deleteRecor();
             },
             onCancel() {
                 console.log('Cancel');
             },
-            
         });
     }
 
@@ -156,7 +152,6 @@ const CardsComponent = () => {
             message.error(`Error: ${error.message}`);
         }
     };
-    console.log(list);
 
     const onSearch = (target) => {
         console.log(target);
@@ -170,10 +165,10 @@ const CardsComponent = () => {
 
     function onChangeCh(e) {
         console.log(`checked = ${e.target.checked}`);
-        
-        if(e.target.checked){
+
+        if (e.target.checked) {
             setGlobal(true);
-          }else{
+        } else {
             setGlobal(false);
         }
     }
@@ -181,9 +176,7 @@ const CardsComponent = () => {
     return (
         <div scroll={{}}>
             <Row>
-                <Space
-                    direction='vertical'
-                    style={{ width: '90%' }}>
+                <Space direction='vertical' style={{ width: '90%' }}>
                     <Search
                         size='large'
                         placeholder='Buscar por titulo de recordatorio'
@@ -201,7 +194,6 @@ const CardsComponent = () => {
                             type='inner'
                             title={
                                 <Row>
-                                    
                                     <Col span={12} style={{ padding: 5 }}>
                                         <h4 class='plistaR'>
                                             {recordatorios.titulo}
@@ -212,7 +204,9 @@ const CardsComponent = () => {
                                             style={{}}
                                             type='primary'
                                             shape='circle'
-                                            onClick={()=>showModal(recordatorios)}
+                                            onClick={() =>
+                                                showModal(recordatorios)
+                                            }
                                             icon={<EditOutlined />}
                                         />
                                     </Col>
@@ -221,28 +215,32 @@ const CardsComponent = () => {
                                             style={{}}
                                             type='primary'
                                             shape='circle'
-                                            onClick={()=>showDeleteConfirm(recordatorios)}
-                                            
+                                            onClick={() =>
+                                                showDeleteConfirm(recordatorios)
+                                            }
                                             icon={<DeleteOutlined />}
                                         />
                                     </Col>
                                 </Row>
                             }>
                             <Row>
-                                
                                 <Col span={4} style={{ padding: 5 }}>
-                                        <Button
-                                            style={{}}
-                                            type='primary'
-                                            shape='circle'
-                                            icon={ recordatorios.global ? <GlobalOutlined/>: <UserOutlined/> }
-                                        />
-                                    
+                                    <Button
+                                        style={{}}
+                                        type='primary'
+                                        shape='circle'
+                                        icon={
+                                            recordatorios.global ? (
+                                                <GlobalOutlined />
+                                            ) : (
+                                                <UserOutlined />
+                                            )
+                                        }
+                                    />
                                 </Col>
                                 <Col span={12}>
                                     <p>Mensaje: {recordatorios.mensaje}</p>
                                     <p>Categoria: {recordatorios.categoria}</p>
-                                    
                                 </Col>
                             </Row>
                         </Card>
@@ -250,70 +248,77 @@ const CardsComponent = () => {
                 ))}
             </Row>
 
-            <Modal title="Actualizar recordatorio" visible={isModalVisible}  onOk={handleOk} onCancel={handleCancel}>
-                
+            <Modal
+                title='Actualizar recordatorio'
+                visible={isModalVisible && seleccionado?.fecha}
+                onOk={handleOk}
+                onCancel={handleCancel}>
                 <Row>
-                <Col span={6} style={{ padding: 16 }}>
-                    <p>Titulo: </p>
-                </Col>
-                <Col span={18} style={{ padding: 16 }}>
-                    
-                    <Input  
-                    defaultValue={seleccionado?.titulo ?? titulo} 
-                    onChange={(e) => setTitulo(e.target.value)}
-                    />
-                </Col>
+                    <Col span={6} style={{ padding: 16 }}>
+                        <p>Titulo: </p>
+                    </Col>
+                    <Col span={18} style={{ padding: 16 }}>
+                        <Input
+                            defaultValue={seleccionado?.titulo ?? titulo}
+                            onChange={(e) => setTitulo(e.target.value)}
+                        />
+                    </Col>
                 </Row>
                 <Row>
-                <Col span={6} style={{ padding: 16 }}>
-                    <p>Descripción (mensaje):</p>
-                </Col>
-                <Col span={18} style={{ padding: 16 }}>
-                <Input placeholder="Descripción del recordatorio"
-                    type="textarea"
-                    autoSize 
-                    onChange={(e) => setMsj(e.target.value)}
-                    value={seleccionado?.mensaje ?? msj}
-                    />
-                <div style={{ margin: '24px 0' }} />
-                </Col>
+                    <Col span={6} style={{ padding: 16 }}>
+                        <p>Descripción (mensaje):</p>
+                    </Col>
+                    <Col span={18} style={{ padding: 16 }}>
+                        <Input
+                            placeholder='Descripción del recordatorio'
+                            type='textarea'
+                            autoSize
+                            onChange={(e) => setMsj(e.target.value)}
+                            value={seleccionado?.mensaje ?? msj}
+                        />
+                        <div style={{ margin: '24px 0' }} />
+                    </Col>
                 </Row>
-                <Checkbox onChange={onChangeCh} defaultChecked={seleccionado?.global ?? global}>Global</Checkbox>
+                <Checkbox
+                    onChange={onChangeCh}
+                    defaultChecked={seleccionado?.global ?? global}>
+                    Global
+                </Checkbox>
                 <Select
-                mode="multiple"
-                style={{ width: '100%' }}
-                placeholder="Seleccionar usuarios"
-                optionLabelProp="label"
-                
-                disabled={(seleccionado?.global ?? global)?true:false}
-                defaultValue={seleccionado?.listUsersPut ?? listUsersPut}
-                
-                > 
-                {listUsers.map(users => (
-                
-                <Option key={users.id} onChange={(e) => setlistUsersput(e.value)}>{}</Option>
-                
-                ))}
-                    
+                    mode='multiple'
+                    style={{ width: '100%' }}
+                    placeholder='Seleccionar usuarios'
+                    optionLabelProp='label'
+                    disabled={seleccionado?.global ?? global ? true : false}
+                    defaultValue={seleccionado?.listUsersPut ?? listUsersPut}>
+                    {listUsers.map((users) => (
+                        <Option
+                            key={users.id}
+                            onChange={(e) => setlistUsersput(e.value)}>
+                            { }
+                        </Option>
+                    ))}
                 </Select>
-                <Select placeholder="Seleccione Categoría" onChange={(value)=>setCategoria(value)} defaultValue={seleccionado?.categoria ?? categoria}  style={{ width: '100%' }} >
-                
-                <Option value="desayuno">Desayuno</Option>
-                <Option value="comida">Comida</Option>
-                <Option value="cena">Cena</Option>
-                <Option value="ejercicio">Ejercicio</Option>
-                <Option value="colacion1">Colacion 1</Option>
-                <Option value="colacion2">Colación 2</Option>
-                <Option value="agua">Agua</Option>
+                <Select
+                    placeholder='Seleccione Categoría'
+                    onChange={(value) => setCategoria(value)}
+                    defaultValue={seleccionado?.categoria ?? categoria}
+                    style={{ width: '100%' }}>
+                    <Option value='desayuno'>Desayuno</Option>
+                    <Option value='comida'>Comida</Option>
+                    <Option value='cena'>Cena</Option>
+                    <Option value='ejercicio'>Ejercicio</Option>
+                    <Option value='colacion1'>Colacion 1</Option>
+                    <Option value='colacion2'>Colación 2</Option>
+                    <Option value='agua'>Agua</Option>
                 </Select>
-                <div className="site-calendar-demo-card">
-                
-                <RangePicker defaultPickerValue={seleccionado?.fecha ?? fecha } />
+                <div className='site-calendar-demo-card'>
+                    <RangePicker
+                        defaultValue={seleccionado?.fecha && [ moment(seleccionado.fecha[ 0 ]), moment(seleccionado.fecha[ seleccionado.fecha.length - 1 ]) ]}
+                    />
                 </div>
-                
-                
             </Modal>
-        </div>        
+        </div>
     );
 };
 
