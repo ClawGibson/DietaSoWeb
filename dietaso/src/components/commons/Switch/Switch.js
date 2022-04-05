ce279158f05be0cc067a9317314dcb46ae409b94
Brodilli <brodilli_@hotmail.com>
2022-04-05 18:41:58 -0500

    Todo bien hasta el momento con la vista de administracion

--- dietaso/src/components/commons/Switch/Switch.js
+++ dietaso/src/components/commons/Switch/Switch.js
@@ -1,10 +1,15 @@
 import React from "react";
-import './Switch.scss'
-const Switch = ({rounded=false})=>{
+import './Switch.scss';
+import cx from 'classnames';
+const Switch = ({rounded=false, isToggle, onToggle})=>{
+
+    const sliderCX = cx('slider', {
+        'rounded': rounded
+    })
     return (
     <label className="switch">
-        <input type='checkbox'/>
-            <span className="slider rounded"/>
+        <input type='checkbox' checked={isToggle} onChange={onToggle}/>
+            <span className={sliderCX}/>
     </label>
     );
 };
--- dietaso/src/components/views/Administracion/index.js
+++ dietaso/src/components/views/Administracion/index.js
@@ -18,42 +18,74 @@ import {
 import './Administracion.scss';
 
 const Administracion = () => {
+
+    const [isInformacionP, setInformacionP]=useState(false);
+    const [isCircunferencia, setCircunferencia]=useState(false);
+    const [isCamposC, setCamposC]=useState(false);
+    const [isEstadoG, setEstadoG]=useState(false);
+    const [isExposicionS, setExposicionS]=useState(false);
+    const [isGastroI, setGastroI]=useState(false);
+    const [isBioquimicos, setBioquimicos]=useState(false);
+    const [isClinicos, setClinicos]=useState(false);
+    const [isSueno, setSueno]=useState(false);
+
+    // const postOpcionesEdicion = async () => {
+    //     const opcionesEdicion= {isInformacionP, isCircunferencia, isCamposC, isEstadoG, isExposicionS, isGastroI, isBioquimicos, isClinicos, isSueno};
+    //     console.log(opcionesEdicion);
+    //     try {
+    //         const response = await apiURL.post('/OpcionesEdicion', opcionesEdicion);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // };
+    //Hacer patch
+
+    console.log(isInformacionP);
+    console.log(isCircunferencia);
+    console.log(isCamposC);
+    console.log(isGastroI);
     return (
         <div className='main-Administracion'>
-
+            <div className='primerosDos'>
             <div className='primero'>
                 <label className="texto">Informacion personal</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isInformacionP={isInformacionP} onToggle={()=> setInformacionP(!isInformacionP)}/>
+                
                 <label class="texto">Circunferencia</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isCircunferencia={isCircunferencia} onToggle={()=> setCircunferencia(!isCircunferencia)}/>
 
                 <label className="texto">Campos corporales</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isCamposC={isCamposC} onToggle={()=> setCamposC(!isCamposC)}/>
 
                 <label className="texto">Estado general</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isEstadoG={isEstadoG} onToggle={()=> setEstadoG(!isEstadoG)}/>
 
                 <label className="texto">Exposicion solar</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isExposicionS={isExposicionS} onToggle={()=> setExposicionS(!isExposicionS)}/>
 
                 <label className="texto">Gastro intestinal</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isGastroI={isGastroI} onToggle={()=> setGastroI(!isGastroI)}/>
 
                 <label className="texto">Bioquimicos</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch"rounded={true} isBioquimicos={isBioquimicos} onToggle={()=> setBioquimicos(!isBioquimicos)}/>
 
                 <label className="texto">Clinicos</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isClinicos={isClinicos} onToggle={()=> setClinicos(!isClinicos)}/>
 
                 <label className="texto">Sueño</label>
-                <Switch className="switch"></Switch>
+                <Switch className="switch" rounded={true} isSueno={isSueno} onToggle={()=> setSueno(!isSueno)}/>
 
             </div>
             <div className='segundo'>
-
+                <label className="texto">OFF</label>
+                <Switch className="switch" rounded={true}/>
+                <label className="texto">ON</label>
+            </div>
             </div>
             <div className='tercero'>
-
+                <label className="texto">OFF</label>
+                <Switch className="switch" rounded={true}/>
+                <label className="texto">ON</label>
             </div>
         </div >
     );
