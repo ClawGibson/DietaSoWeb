bd6628ef6a5b32dfd9ad1f9e2a4a657bb8402e42
Gama <gamalielbj@gmail.com>
2022-04-09 11:35:40 -0500

    Update

--- dietaso/src/components/commons/Switch/Switch.js
+++ dietaso/src/components/commons/Switch/Switch.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React from 'react';
 import './Switch.scss';
 import cx from 'classnames';
-const Switch = ({rounded=false, isToggle, onToggle})=>{
-
+const Switch = ({ rounded = false, isToggle, onToggle, checked }) => {
     const sliderCX = cx('slider', {
-        'rounded': rounded
-    })
+        rounded: rounded,
+    });
     return (
-    <label className="switch">
-        <input type='checkbox' checked={isToggle} onChange={onToggle}/>
-            <span className={sliderCX}/>
-    </label>
+        <label className='switch'>
+            <input
+                type='checkbox'
+                checked={checked ?? isToggle}
+                onChange={onToggle}
+            />
+            <span className={sliderCX} />
+        </label>
     );
 };
 export default Switch;
--- dietaso/src/components/views/Administracion/index.js
+++ dietaso/src/components/views/Administracion/index.js
@@ -1,24 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Col, Input, Row, Button, Modal, Select } from 'antd';
-import Switch from '../../commons/Switch/Switch';
-// import UserCard from '../../commons/UserCard/UserCard';
+import React, { useState, useEffect, useRef } from 'react';
 
-// import { addAuthorizationAction } from '../../../redux/actions/authorizationAction';
+//import Switch from '../../commons/Switch/Switch';
 import apiURL from '../../../axios/axiosConfig';
 
-import {
-    PlusOutlined,
-    ExclamationCircleOutlined,
-    DeleteOutlined,
-    EditOutlined,
-    GlobalOutlined,
-    UserOutlined,
-} from '@ant-design/icons';
+import { Switch } from 'antd';
 
 import './Administracion.scss';
 
 const Administracion = () => {
-
     const [isInformacionP, setInformacionP] = useState(false);
     const [isCircunferencia, setCircunferencia] = useState(false);
     const [isCamposC, setCamposC] = useState(false);
@@ -29,16 +18,45 @@ const Administracion = () => {
     const [isClinicos, setClinicos] = useState(false);
     const [isSueno, setSueno] = useState(false);
 
+    const infoRef = useRef(false);
+    const circunferenciaRef = useRef(false);
+    const camposCRef = useRef(false);
+    const estadoGRef = useRef(false);
+    const exposicionSRef = useRef(false);
+    const gastroIRef = useRef(false);
+    const bioquimicosRef = useRef(false);
+    const clinicosRef = useRef(false);
+    const suenoRef = useRef(false);
+
     useEffect(() => {
         getOpcionesEdicion();
-        
     }, []);
 
     const getOpcionesEdicion = async () => {
         try {
             const { data } = await apiURL.get('/opcionesEdicion');
             console.log(data);
-            const{bioquimicos,camposCorporales,circunferencia,clinicos,estadoGeneral,exposicionSolar,gastroIntestinal,informacionPersonal,sueno}=data[0];
+            const {
+                bioquimicos,
+                camposCorporales,
+                circunferencia,
+                clinicos,
+                estadoGeneral,
+                exposicionSolar,
+                gastroIntestinal,
+                informacionPersonal,
+                sueno,
+            } = data[0];
+            infoRef.current = informacionPersonal;
+            circunferenciaRef.current = circunferencia;
+            camposCRef.current = camposCorporales;
+            estadoGRef.current = estadoGeneral;
+            exposicionSRef.current = exposicionSolar;
+            gastroIRef.current = gastroIntestinal;
+            bioquimicosRef.current = bioquimicos;
+            clinicosRef.current = clinicos;
+            suenoRef.current = sueno;
+            setInformacionP(informacionPersonal);
             setBioquimicos(bioquimicos);
             setCamposC(camposCorporales);
             setCircunferencia(circunferencia);
@@ -48,7 +66,6 @@ const Administracion = () => {
             setGastroI(gastroIntestinal);
             setInformacionP(informacionPersonal);
             setSueno(sueno);
-            
         } catch (error) {
             console.groupCollapsed('Error xd');
             console.error(error);
@@ -58,7 +75,6 @@ const Administracion = () => {
 
     const handlePatch = async (props) => {
         try {
-
             const body = getCurrentBody(props.key, props.value);
 
             const { data } = await apiURL.patch('opcionesEdicion', body);
@@ -69,30 +85,43 @@ const Administracion = () => {
     };
 
     const getCurrentBody = (key, value) => {
-
         switch (key) {
             case 1:
-                return { 'informacionPersonal': value }
-
+                infoRef.current = value;
+                setInformacionP(value);
+                return { informacionPersonal: value };
             case 2:
-                return { 'circunferencia': value }
+                circunferenciaRef.current = value;
+                setCircunferencia(value);
+                return { circunferencia: value };
             case 3:
-                return { 'camposCorporales': value }
-
+                camposCRef.current = value;
+                setCamposC(value);
+                return { camposCorporales: value };
             case 4:
-                return { 'estadoGeneral': value }
+                estadoGRef.current = value;
+                setEstadoG(value);
+                return { estadoGeneral: value };
             case 5:
-                return { 'exposicionSolar': value }
-
+                exposicionSRef.current = value;
+                setExposicionS(value);
+                return { exposicionSolar: value };
             case 6:
-                return { 'gastroIntestinal': value }
+                gastroIRef.current = value;
+                setGastroI(value);
+                return { gastroIntestinal: value };
             case 7:
-                return { 'bioquimicos': value }
+                bioquimicosRef.current = value;
+                setBioquimicos(value);
+                return { bioquimicos: value };
             case 8:
-                return { 'clinicos': value }
-
+                clinicosRef.current = value;
+                setClinicos(value);
+                return { clinicos: value };
             case 9:
-                return { 'sueno': value }
+                suenoRef.current = value;
+                setSueno(value);
+                return { sueno: value };
             default:
                 break;
         }
@@ -102,46 +131,99 @@ const Administracion = () => {
         <div className='main-Administracion'>
             <div className='primerosDos'>
                 <div className='primero'>
-                    <label className="texto">Informacion personal</label>
-                    <Switch className="switch" rounded={true} checked={isInformacionP} onToggle={(s) => handlePatch({ key: 1, value: s.target.checked })} />
+                    <label className='texto'>Informacion personal</label>
+                    <Switch
+                        ref={infoRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isInformacionP || infoRef.current}
+                        onChange={(s) => handlePatch({ key: 1, value: s })}
+                    />
 
-                    <label class="texto">Circunferencia</label>
-                    <Switch className="switch" rounded={true} checked={isCircunferencia} onToggle={(s) => handlePatch({ key: 2, value: s.target.checked })} />
+                    <label class='texto'>Circunferencia</label>
+                    <Switch
+                        ref={circunferenciaRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isCircunferencia || circunferenciaRef.current}
+                        onChange={(s) => handlePatch({ key: 2, value: s })}
+                    />
 
-                    <label className="texto">Campos corporales</label>
-                    <Switch className="switch" rounded={true} checked={isCamposC} onToggle={(s) => handlePatch({ key: 3, value: s.target.checked })} />
+                    <label className='texto'>Campos corporales</label>
+                    <Switch
+                        ref={camposCRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isCamposC || camposCRef.current}
+                        onChange={(s) => handlePatch({ key: 3, value: s })}
+                    />
 
-                    <label className="texto">Estado general</label>
-                    <Switch className="switch" rounded={true} checked={isEstadoG} onToggle={(s) => handlePatch({ key: 4, value: s.target.checked })} />
+                    <label className='texto'>Estado general</label>
+                    <Switch
+                        ref={estadoGRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isEstadoG || estadoGRef.current}
+                        onChange={(s) => handlePatch({ key: 4, value: s })}
+                    />
 
-                    <label className="texto">Exposicion solar</label>
-                    <Switch className="switch" rounded={true} checked={isExposicionS} onToggle={(s) => handlePatch({ key: 5, value: s.target.checked })} />
+                    <label className='texto'>Exposicion solar</label>
+                    <Switch
+                        ref={exposicionSRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isExposicionS || exposicionSRef.current}
+                        onChange={(s) => handlePatch({ key: 5, value: s })}
+                    />
 
-                    <label className="texto">Gastro intestinal</label>
-                    <Switch className="switch" rounded={true} checked={isGastroI} onToggle={(s) => handlePatch({ key: 6, value: s.target.checked })} />
+                    <label className='texto'>Gastro intestinal</label>
+                    <Switch
+                        ref={gastroIRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isGastroI || gastroIRef.current}
+                        onChange={(s) => handlePatch({ key: 6, value: s })}
+                    />
 
-                    <label className="texto">Bioquimicos</label>
-                    <Switch className="switch" rounded={true} checked={isBioquimicos} onToggle={(s) => handlePatch({ key: 7, value: s.target.checked })} />
+                    <label className='texto'>Bioquimicos</label>
+                    <Switch
+                        ref={bioquimicosRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isBioquimicos || bioquimicosRef.current}
+                        onChange={(s) => handlePatch({ key: 7, value: s })}
+                    />
 
-                    <label className="texto">Clinicos</label>
-                    <Switch className="switch" rounded={true} checked={isClinicos} onToggle={(s) => handlePatch({ key: 8, value: s.target.checked })} />
+                    <label className='texto'>Clinicos</label>
+                    <Switch
+                        ref={clinicosRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isClinicos || clinicosRef.current}
+                        onChange={(s) => handlePatch({ key: 8, value: s })}
+                    />
 
-                    <label className="texto">Sueño</label>
-                    <Switch className="switch" rounded={true} checked={isSueno} onToggle={(s) => handlePatch({ key: 9, value: s.target.checked })} />
-
+                    <label className='texto'>Sueño</label>
+                    <Switch
+                        ref={suenoRef}
+                        className='switch'
+                        rounded={true}
+                        checked={isSueno || suenoRef.current}
+                        onChange={(s) => handlePatch({ key: 9, value: s })}
+                    />
                 </div>
                 <div className='segundo'>
-                    <label className="texto">OFF</label>
-                    <Switch className="switch" rounded={true} />
-                    <label className="texto">ON</label>
+                    <label className='texto'>OFF</label>
+                    <Switch className='switch' rounded={true} />
+                    <label className='texto'>ON</label>
                 </div>
             </div>
             <div className='tercero'>
-                <label className="texto">OFF</label>
-                <Switch className="switch" rounded={true} />
-                <label className="texto">ON</label>
+                <label className='texto'>OFF</label>
+                <Switch className='switch' rounded={true} />
+                <label className='texto'>ON</label>
             </div>
-        </div >
+        </div>
     );
 };
 
