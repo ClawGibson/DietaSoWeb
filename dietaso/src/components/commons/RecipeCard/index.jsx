27c3ed09930a8f81feaf1d2732a65d537568b30c
Gama <gamalielbj@gmail.com>
2022-05-18 21:28:58 -0500

    Refactoring recipes

--- dietaso/src/components/commons/RecipeCard/index.jsx
+++ dietaso/src/components/commons/RecipeCard/index.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
-
-import { Card, Row, Col, Button } from 'antd';
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
 import YouTube from '@u-wave/react-youtube';
 
 import UploadImgs from '../../commons/UploadImgs';
+import HeaderTitle from './components/Header';
+import { getUrlsID } from '../../../utils';
+import styles from './RecipesCard.module.scss';
 
 const RecipesCard = ({ recipe, onEdit, onDelete, onUploadImg }) => {
     const [url, setUrl] = useState(recipe?.foto ?? '');
     const [key, setKey] = useState('');
+
+    const onUploadImgDisabled = recipe?.foto;
+    const recipePhotoUrl = recipe?.foto ?? url;
+    const description = recipe?.descripcion ?? 'Aún no hay información';
 
     useEffect(() => {
         return () => {
@@ -21,19 +26,6 @@ const RecipesCard = ({ recipe, onEdit, onDelete, onUploadImg }) => {
         return recipe ? '★ Receta destacada' : '';
     };
 
-    const getUrlsID = (URL) => {
-        const ID = URL.match(
-            /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/
-        );
-        if (ID !== null) {
-            //console.log(ID[1]);
-            //setVideosData([...videosData, { id: 2, link: ID[1] }]);
-            return ID[1];
-        } else {
-            console.log('The youtube url is not valid');
-        }
-    };
-
     const onUpload = (values) => {
         setUrl(values.url);
         setKey(values.key);
@@ -41,44 +33,13 @@ const RecipesCard = ({ recipe, onEdit, onDelete, onUploadImg }) => {
     };
 
     return (
-        <Card
-            title={
-                <Row>
-                    <Col span={18} style={{ padding: 5 }}>
-                        {recipe.titulo}
-                    </Col>
-                    <Col>
-                        <Button
-                            style={{}}
-                            type='primary'
-                            shape='circle'
-                            icon={<EditOutlined />}
-                            onClick={() => onEdit(recipe)}
-                        />
-                    </Col>
-                    <Col>
-                        <Button
-                            style={{}}
-                            type='primary'
-                            shape='circle'
-                            icon={<DeleteOutlined />}
-                            onClick={() => onDelete(recipe)}
-                        />
-                    </Col>
-                </Row>
-            }>
-            <UploadImgs
-                disabled={recipe?.foto ? true : false}
-                onChange={onUpload}
-                url={recipe?.foto ?? url}
-            />
+        <Card className={styles.container} title={<HeaderTitle recipe={recipe} onEdit={onEdit} onDelete={onDelete} />}>
+            <UploadImgs disabled={onUploadImgDisabled} onChange={onUpload} url={recipePhotoUrl} />
             <div>
                 <YouTube video={getUrlsID(recipe.url)} autoplay={false} />
             </div>
-            <div className='sc_recipes_destacado'>
-                {`${isStar(recipe.destacado)}`}
-            </div>
-            <div>{recipe?.descripcion ?? 'Aún no hay información'}</div>
+            <div>{`${isStar(recipe.destacado)}`}</div>
+            <div>{description}</div>
         </Card>
     );
 };
